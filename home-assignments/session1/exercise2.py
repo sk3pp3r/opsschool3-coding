6491eb3a1a7779262de461df8ae013f168c791eb
Haim Cohen <haim1979@gmail.com>
2018-11-04 22:11:34 +0200

    Update and rename exercise2.txt to exercise2.py

--- home-assignments/session1/exercise2.py
+++ home-assignments/session1/exercise2.py
@@ -1,3 +1,4 @@
+'''
 1.
 Write a python program, that checks your location according to your IP.
 Then checks the current weather at your location and writes the result to a file in a regular text format.
@@ -6,5 +7,12 @@ Then checks the current weather at your location and writes the result to a file
 In that same program, create a list with at least 10 cities,
 And print their current weather in the following format:
 “The weather in <city>, <country>(full country name) is XX degrees.
+'''
+import requests 
+from requests import get
 
+
+location_by_ip_api = get('http://ip-api.com/json').text
+geo_list = str(location_by_ip_api)
+print(geo_list)
 
