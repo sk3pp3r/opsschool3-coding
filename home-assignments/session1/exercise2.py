592daa9e9c1eac7922dbd8005620a62ea9e1c105
Haim Cohen <haim1979@gmail.com>
2018-11-06 17:39:48 +0200

    Update exercise2.py

--- home-assignments/session1/exercise2.py
+++ home-assignments/session1/exercise2.py
@@ -8,18 +8,22 @@ In that same program, create a list with at least 10 cities,
 And print their current weather in the following format:
 “The weather in <city>, <country>(full country name) is XX degrees.
 '''
-# import necessary modules
+# IMPORT NECESSARY MODULES
 import re
 import json
 from urllib.request import urlopen
 
-# set varibles
-url = 'http://ipinfo.io/json'
-response = urlopen(url)
-data = json.load(response)
+# SET VARIBLES
+###  GEOLOCATION
+ipurl = 'http://ipinfo.io/json' 
+ipresponse = urlopen(ipurl)
+ipdata = json.load(ipresponse)
+city = ipdata['city']
+country=ipdata['country']
 
-city = data['city']
-country=data['country']
+### WEATHERMAP
+weather_api_url = 'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID=B0f3f7f5f01d058bc63e8c2d7301d805'
+
 
 print('Region : {} \nCity  : {}'.format(country,city))
 
